544a84c76d0f618dc71217c70e7b51069de07be3
KToppo <kalyan.toppo.2021@gmail.com>
2025-08-08 19:22:15 +0530

    update GUI in app.py

--- book_recomendation_system/app.py
+++ book_recomendation_system/app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pickle
 import pandas as pd
-import requests
+import numpy as np
 
 top50 = pickle.load(open('top50books.pkl', 'rb'))
 top50 = pd.DataFrame(top50)
@@ -10,18 +10,23 @@ top50 = pd.DataFrame(top50)
 matrix = pickle.load(open('user_matrix.pkl', 'rb'))
 matrix = pd.DataFrame(matrix)
 
+
+similarity = pickle.load(open('similarity.pkl', 'rb'))
+
+def recommend(book_name):
+    index = np.where(matrix.index==book_name)[0][0]
+    distances = similarity[index]
+    similar_items = sorted(list(enumerate(distances)),key=lambda x:x[1], reverse=True)
+
+    recommended_book = []
+    for i in similar_items[1:6]:
+        recommended_book.append(matrix.index[i[0]])
+    return recommended_book
+
 def main():
     left, right = st.columns(2)
-    left.button("Top 50", use_container_width=True, type="primary")
-    if right.button("Recommend", use_container_width=True):
-        selected_book_name = st.selectbox(
-                            "Select the book you like",
-                            matrix.index,
-                        )
-        if st.button("Recommend"):
-            st.text(selected_book_name)
-            
-    else:
+    left.button("Recommend", use_container_width=True, type="primary")
+    if right.button("Top 50", use_container_width=True):
         st.title('Top 50 Books')
         names = top50['Book-Title']
         authors = top50['Book-Author']
@@ -41,6 +46,17 @@ def main():
                 st.write("")
             start += 5
             end += 5
+            
+    else:
+        selected_book_name = st.selectbox(
+                            "Select the book you like",
+                            matrix.index,
+                        )
+        if st.button("Recommend"):
+            # st.text(selected_book_name)
+            recommended = recommend(selected_book_name)
+            for i in recommended:
+                st.text(i)
     pass
 
 
